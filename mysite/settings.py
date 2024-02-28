77bdd924d0cdc0c97988976ea383501462d28358
Basile REINERT <basile.reinert@seenovate.com>
2024-02-28 09:33:45 +0100

    probleme

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
