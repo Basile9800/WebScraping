cfdc5c04c374bf599d6ae9a9e400f4911f1d4a4e
Basile REINERT <basile.reinert@seenovate.com>
2024-03-05 16:21:54 +0100

    alle

--- blog/views.py
+++ blog/views.py
@@ -10,14 +10,21 @@ from django.http import JsonResponse
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     csv_import = post.csv_file
+    categories = set()
+    posts = Post.objects.all()
     filtered_data = None
     name = None
     if request.method == 'POST':
         name = request.POST.get('name', '')
         if post.csv_file:
             filtered_data = post.filter_csv_data_by_name(name)
-    
-    return render(request, 'blog/post_detail.html', {'post': post, 'csv_import': csv_import, 'filtered_data': filtered_data, 'name': name})
+    for post in posts:
+        if post.csv_file:
+            csv_data = post.read_csv_data()
+            for row in csv_data:
+                if len(row) > 3:
+                    categories.add(row[3])
+    return render(request, 'blog/post_detail.html', {'post': post, 'categories': categories, 'csv_import': csv_import, 'filtered_data': filtered_data, 'name': name})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
