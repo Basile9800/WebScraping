4c7c23f515d3ab2b52f82cd6f1a91621ce1da3d2
Basile REINERT <basile.reinert@seenovate.com>
2024-03-05 12:00:04 +0100

    ajout stat

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from .forms import PostForm 
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import logging
 
 # Create your views here.
 
@@ -35,57 +35,52 @@ def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def search_by_name(request):
     if request.method == 'POST':
         name = request.POST.get('name', '')
-        search_results = {}
+        search_results = []
         posts = Post.objects.all()
         for post in posts:
             if post.csv_file:
                 csv_data = post.read_csv_data()
                 for row in csv_data:
                     if len(row) > 1 and name.lower() in row[1].lower():
-                        if post.title not in search_results:
-                            search_results[post.title] = set()
-                        search_results[post.title].add(tuple(row))               
-        for title, rows_set in search_results.items():
-            search_results[title] = list(rows_set)
-        return render(request, 'blog/search_results.html', {'search_results': search_results, 'name': name})
-    return render(request, 'search_form.html')
+                        search_results.append(row)  # Ajouter la ligne correspondante aux résultats de recherche
+        return JsonResponse({'search_results': search_results})  # Renvoyer les résultats de la recherche en JSON
+    return JsonResponse({'error': 'Méthode de requête invalide'}, status=400)
+
+
+
+logger = logging.getLogger(__name__)
 
 def calculate_averages(request):
-    if request.method == 'POST':
-        selected_category = request.POST.get('selected_col4_value')
-        if selected_category:
-            total_speed = 0
-            count = 0
-            
-            # Récupération des données des posts avec des fichiers CSV
-            posts = Post.objects.filter(csv_file__isnull=False)
-            for post in posts:
-                csv_data = post.read_csv_data()
-                for row in csv_data:
-                    # Vérification de la catégorie et calcul de la moyenne de vitesse
-                    if len(row) > 10 and row[3] == selected_category:
-                        try:
-                            total_speed += float(row[10])
-                            count += 1
-                        except ValueError:
-                            pass  # Ignorer les valeurs non numériques
-            
-            # Calcul de la moyenne de vitesse
-            if count > 0:
-                average_speed = total_speed / count
-                # Créez le HTML à renvoyer
-                html_response = f"<p>L'average speed pour la catégorie {selected_category} est {average_speed}</p>"
-                # Renvoyez une réponse HTTP avec le HTML
-                return HttpResponse(html_response)
+    if request.method == 'GET':
+        logger.debug("Calculating averages")
+        categories = set()
         
-        # Si aucune catégorie n'est sélectionnée ou si aucune donnée n'est trouvée, renvoyer une réponse d'erreur
-        return HttpResponse('<p>Erreur : catégorie invalide ou pas de données trouvées.</p>', status=400)
+        # Récupération des données des posts avec des fichiers CSV
+        posts = Post.objects.filter(csv_file__isnull=False)
+        for post in posts:
+            csv_data = post.read_csv_data()
+            for row in csv_data:
+                # Vérification de la présence de la colonne de catégorie
+                if len(row) > 3:
+                    categories.add(row[3])  # Ajouter la catégorie à l'ensemble
+        
+        # Créer une liste à partir de l'ensemble pour pouvoir la retourner en JSON
+        category_list = list(categories)
+        logger.debug("Categories: %s", category_list)
+        
+        # Renvoyer les catégories en JSON
+        return JsonResponse(category_list, safe=False)
+    
+    # Si la méthode de la requête n'est pas GET, renvoyer une réponse d'erreur
+    return JsonResponse({'error': 'Méthode de requête invalide'}, status=400)
 
-    # Si la méthode de la requête n'est pas POST, renvoyer une réponse d'erreur
-    return HttpResponse('<p>Erreur : méthode de requête invalide.</p>', status=400)
+
+
+
 
                     
 
